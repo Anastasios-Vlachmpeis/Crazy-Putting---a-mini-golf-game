b77be0963e3099904851ece7c40d74840388c72e
Volovei, Damian (Stud. DACS) <d.volovei@student.maastrichtuniversity.nl>
2026-04-05 15:02:47 +0200

    fixed the infinite golf simulation loop and add trimmed result array

--- src/main/java/Experiment/MotionExperiment.java
+++ src/main/java/Experiment/MotionExperiment.java
@@ -8,18 +8,19 @@ import Solvers.RungeKuttaSolver;
 import Systems.GolfODE;
 
 public class MotionExperiment {
+
     public static void main(String[] args) {
-        double[] y0 = new double[] { 3, 2, 4, 3 };
+        double[] y0 = new double[] { 0, 0, 4, -4 };
         Ball ball = new Ball(y0);
         CourseProfile courseSettings = new CourseProfile(0.07, 0.15);
         GolfODE course = new GolfODE(courseSettings);
         RungeKuttaSolver solver = new RungeKuttaSolver();
 
-        double[][] results = solver.solveBall(course, ball.getState(), 0.5);
+        double[][] results = solver.solveBall(course, ball.getState(), 1);
 
-        // for (double[] row : results) {
-        //     System.out.println(Arrays.toString(row));
-        // }
+        for (double[] row : results) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 
 }
